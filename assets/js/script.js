c11ee9a47b638b6964554bd70489bd2a72d2b729
CPalileo <cpalileo@gmail.com>
2022-01-26 21:36:21 -0800

    screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,12 +74,19 @@ var quizQuestions = [
 ];
 
 // define variables for classes in html file.  create variable for questions list
-var buttonStart = document.querySelector(".buttonStart");
-var welcome = document.querySelector(".welcome");
-var questionDisplay = document.querySelector(".textQuestion");
+var welcomeScreen = document.querySelector("#welcomeScreen");
+var challenge = document.querySelector(".challenge");
+var startButton = document.querySelector(".startButton");
+var quizBlock = document.querySelector("#quizBlock");
+var questionDisplay = document.querySelector(".questionDisplay");
 var textAnswer = document.querySelector(".textAnswer");
-var displayScore = document.querySelector(".currentScore");
-var endGameScreen = document.querySelector(".endGame");
+var displayScore = document.querySelector(".displayScore");
+var endGameScreen = document.querySelector("#endGameScreen");
+var gameOverText = document.querySelector(".gameOverText");
+var resetButton = document.querySelector(".resetButton");
+var finalScore = document.querySelector("#finalScore");
+var timerRules;
+var stopTimer;
 // var questionCount = quizQuestions.length - 1; // was receiving an error because quizQuestions.length was past the end of the array.  So needed to subtract 1 form the array length.
 var questionCount = 0;
 var trackPoints = 0;
@@ -88,83 +95,105 @@ var timer = 60;
 
 // display high score at upper left screen
 
-buttonStart.addEventListener("click", gameStart); //event listener, button reacts to mouse click
+function welcome() {
+  // endGameScreen.textContent = ""; // clears endGameScreen from previous game
+  challenge.textContent =
+    "Welcome to the Marvel Comics quiz challenge!  Are you ready to test your knowledge of Marvel Comics?  Take this short quiz that separates mainstream fans from the Marvel experts.";
+  buttonStart();
 
-function gameStart() {
-  // function activated from button click
-  welcome.textContent = ""; // clears the welcome screen
-  countDown(); // starts the timer
-  quiz(); // starts the quiz function
-}
+  function buttonStart() {
+    var generateStart = document.createElement("Button"); // allows for the dynamic creation of the choice buttons in the HTML
+    startButton.setAttribute("class", "buttonColors"); // sets the colors of the button choices
+    startButton.textContent = "Accept Challenge";
+    generateStart.appendChild(startButton);
+    welcomeScreen.appendChild(generateStart);
+    startButton.addEventListener("click", gameStart); // adds the event listener to listen which button choice is clicked
+  }
 
-// create a timer (Get the questions before working on timer)
-function countDown() {
-  var timerRules = setInterval(function () {
-    document.getElementById("progBar").value = 60 - --timer; // subtracts 1 second from 30
-    document.getElementById("counting").innerHTML = timer; // prints the seconds remaining
-    if (timer <= 0) {
-      clearInterval(timerRules); // prevents a negative count down
-      gameOver(timerRules);
+  function gameStart() {
+    // var elements = document.getElementById("welcomeScreen");
+    welcomeScreen.remove();
+    countDown(); // starts the timer
+    quiz(); // starts the quiz function
+  }
+
+  // create a timer (Get the questions before working on timer)
+  function countDown() {
+    var timerRules = setInterval(function () {
+      document.getElementById("progBar").value = 60 - --timer; // subtracts 1 second from 60
+      document.getElementById("counting").innerHTML = timer; // prints the seconds remaining
+      if (timer <= 0) {
+        clearInterval(timerRules); // prevents a negative count down
+        gameOver();
+      } else if (stopTimer === 0) {
+        clearInterval(timerRules);
+      }
+    }, 1000); // set to 1 second
+  }
+
+  function quiz() {
+    textAnswer.textContent = ""; // clears screen prior to loading each question
+    questionDisplay.textContent = questionStaged.question; // the text content for questionDisplay (which prints to the HTML), pulls the question from the question list
+    questionStaged.option.forEach(function (choice) {
+      // loops the function for each item in the array
+      // the relevant answer "option[s]" are pulled and assigned a button with attributes and listeners for each "choice"
+      var choiceButton = document.createElement("Button"); // allows for the dynamic creation of the choice buttons in the HTML
+      choiceButton.setAttribute("class", "buttonColors"); // sets the colors of the button choices
+      choiceButton.textContent = choice; // the option text from the quiz questions is written in each button choice
+      textAnswer.append(choiceButton); // sends the text of the answer choices to the HTML
+      choiceButton.setAttribute("points", choiceButton.textContent); // places the questions point with the answers
+      choiceButton.addEventListener("click", questionRules); // adds the event listener to listen which button choice is clicked
+    });
+  }
+
+  // if then loops.  correct adds to score and pulls next question.  incorrect deducts from time and
+  //pulls next question
+
+  function questionRules(button) {
+    if (button.srcElement.innerText === questionStaged.answer) {
+      // if a correct answer is given
+      trackPoints++; //score will increase the points based on the value above
+      displayScore.textContent = trackPoints + " Points"; // displays the updated points in the element
+      localStorage.setItem("pointTally", trackPoints); // stores points in local storage
+    } else {
+      // if wrong answer is selected
+      timer -= 5; // penalize 5 seconds
     }
-  }, 1000); // set to 1 second
-}
-
-function quiz() {
-  textAnswer.textContent = ""; // clears screen prior to loading each question
-  questionDisplay.textContent = questionStaged.question; // the text content for questionDisplay (which prints to the HTML), pulls the question from the question list
-  questionStaged.option.forEach(function (choice) {
-    // loops the function for each item in the array
-    // the relevant answer "option[s]" are pulled and assigned a button with attributes and listeners for each "choice"
-    var choiceButton = document.createElement("Button"); // allows for the dynamic creation of the choice buttons in the HTML
-    choiceButton.setAttribute("class", "buttonColors"); // sets the colors of the button choices
-    choiceButton.textContent = choice; // the option text from the quiz questions is written in each button choice
-    textAnswer.append(choiceButton); // sends the text of the answer choices to the HTML
-    choiceButton.setAttribute("points", choiceButton.textContent); // places the questions point with the answers
-    choiceButton.addEventListener("click", questionRules); // adds the event listener to listen which button choice is clicked
-  });
-}
-
-// if then loops.  correct adds to score and pulls next question.  incorrect deducts from time and
-//pulls next question
-
-function questionRules(button) {
-  console.log(button.srcElement.innerText);
-  if (button.srcElement.innerText === questionStaged.answer) {
-    console.log("correct answer");
-    // if a correct answer is given
-    trackPoints++; //score will increase the points based on the value above
-    displayScore.textContent = trackPoints + " Points"; // displays the updated points in the element
-    localStorage.setItem("pointTally", trackPoints); // stores points in local storage
-  } else {
-    console.log("incorrect answer");
-    // if wrong answer is selected
-    timer -= 5; // penalize 5 seconds
+    questionCount++; // moves to the next question
+    if (questionCount >= quizQuestions.length) {
+      // personal note: questionCount is past the last question because length of array is one greater than the final item's index
+      // if all the questions are answered  move to game over
+      gameOver();
+    } else {
+      console.log(questionCount);
+      questionStaged = quizQuestions[questionCount]; // pulls the questions from the pool
+      quiz(questionStaged); // if more questions remain the loop will continue
+    }
   }
-  questionCount++; // moves to the next question
-  if (questionCount >= quizQuestions.length) {
-    // personal note: questionCount is past the last question because length of array is one greater than the final item's index
-    // if all the questions are answered  move to game over
-    gameOver();
-  } else {
-    console.log(questionCount);
-    questionStaged = quizQuestions[questionCount]; // pulls the questions from the pool
-    quiz(questionStaged); // if more questions remain the loop will continue
+
+  // if all questions correct then win, if timer runs out then game over
+  function gameOver() {
+    console.log("gameover");
+    quizBlock.textContent = "";
+    stopTimer = 0;
+    gameOverText.textContent = "GAME OVER";
+    finalScore.textContent = "You scored: " + trackPoints + " points";
+
+    // if play again then reset game
+    buttonReset();
+    function buttonReset() {
+      var generateReset = document.createElement("Button"); // allows for the dynamic creation of the choice buttons in the HTML
+      resetButton.setAttribute("class", "buttonColors"); // sets the colors of the button choices
+      resetButton.textContent = "Play Again?";
+      generateReset.appendChild(resetButton);
+      endGameScreen.appendChild(generateReset);
+      resetButton.addEventListener("click", welcome); // adds the event listener to listen which button choice is clicked
+    }
   }
 }
 
-// if all questions correct then win, if timer runs out then game over
-function gameOver(timerRules) {
-  console.log("YOU MADE IT TO THE GAME OVER SECTION");
+welcome();
 
-  questionDisplay.style.visibility = "hidden";
-  textAnswer.style.visibility = "hidden";
-
-  endGameScreen.style.visibility = "visible";
-  endGameScreen.textContent = displayScore + " points";
-  clearInterval(timerRules);
-}
 // current score is compared to high score.  if current score is > than high score then
-//congratulate & announce high score, input initials, store score in local storage
-// if current score is < high score, report score and ask if want to play again or quit
-// if play again then reset game
-// if quit then main menu
+// congratulate & announce high score, input initials, store score in local storage
+// if current score is < high score, report score
